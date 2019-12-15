0f732e3bcc8a0eae60fc74efbd031ed0e005d33c
Brian Christopher <bcwood79@yahoo.com>
2019-12-14 22:48:27 -0500

    Added jquery, and javascript file.

---
+++ assets/javascript/WordGame.js
@@ -0,0 +1,10 @@
+//GLOBAL VARIABLES
+let playerName = "Mickey Mouse";
+let wins = 0;
+let losses = 0;
+let badGuessesRemaining = 14;
+
+console.log("Player Name: ", playerName, " | Wins: ", wins);
+
+//let nameHolder = $("span").text("Cheese"); 
+$("#playerName").text(playerName);
